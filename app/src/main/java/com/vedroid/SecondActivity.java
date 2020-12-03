40e57a38b4b60b4f843a78a49bf23cabaeee9e44
SamuilOlegovich <47562089+SamuilOlegovich@users.noreply.github.com>
2020-12-03 12:27:58 +0200

    Куа

--- app/src/main/java/com/vedroid/SecondActivity.java
+++ app/src/main/java/com/vedroid/SecondActivity.java
@@ -23,6 +23,7 @@ public class SecondActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
